54a1912063bafb08487c77ffb444cf166189b680
Jasperi Juutinen <82633996+Jaju1@users.noreply.github.com>
2023-11-20 18:42:46 +0200

    Add files via upload

---
+++ mavic4/controllers/mavic2pro/build/release/mavic2pro.d
@@ -0,0 +1,13 @@
+build/release/mavic2pro.o: mavic2pro.c \
+ C:\Users\jaspe\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\jaspe\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\jaspe\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\jaspe\AppData\Local\Programs\Webots/include/controller/c/webots/camera.h \
+ C:\Users\jaspe\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Users\jaspe\AppData\Local\Programs\Webots/include/controller/c/webots/compass.h \
+ C:\Users\jaspe\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h \
+ C:\Users\jaspe\AppData\Local\Programs\Webots/include/controller/c/webots/gyro.h \
+ C:\Users\jaspe\AppData\Local\Programs\Webots/include/controller/c/webots/inertial_unit.h \
+ C:\Users\jaspe\AppData\Local\Programs\Webots/include/controller/c/webots/keyboard.h \
+ C:\Users\jaspe\AppData\Local\Programs\Webots/include/controller/c/webots/led.h \
+ C:\Users\jaspe\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h
