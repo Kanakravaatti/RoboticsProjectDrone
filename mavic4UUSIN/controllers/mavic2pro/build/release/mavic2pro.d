3dade4dcfd74a850e2d74ca1f18dfc2ca28f32b2
Olli Heikkinen <82203174+OlliHeik@users.noreply.github.com>
2023-12-11 22:46:57 +0200

    Add files via upload

---
+++ mavic4UUSIN/controllers/mavic2pro/build/release/mavic2pro.d
@@ -0,0 +1,18 @@
+build/release/mavic2pro.o: mavic2pro.c \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/camera.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/compass.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/gyro.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/inertial_unit.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/keyboard.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/led.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/lidar.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/lidar_point.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/brake.h \
+ C:\Users\Olli\AppData\Local\Programs\Webots/include/controller/c/webots/position_sensor.h
